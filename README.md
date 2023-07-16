5c16593c86bde27446de06cc52b639ee7e2ead54
Eric Huss <eric@huss.org>
2023-07-16 14:42:25 -0700

    Fail on merge to main

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # test
 
-Change directly on main.
-
 Test3
--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Test script"
+
+if grep -q "on main" README.md
+then
+    echo Should fail if on main
+    exit 1
+else
+    echo Not on main, passing.
+fi
